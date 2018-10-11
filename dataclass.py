6e2f602d4e5a7f9206dab56e246a0ea98f99a861
Vitaly Potyarkin <sio.wtf@gmail.com>
2018-10-11 17:37:29 +0300

    Layout for data classes

---
+++ dataclass.py
@@ -0,0 +1,29 @@
+'''
+Classes for the structured data
+'''
+
+class TreeStructuredData:
+    '''
+    Generic metadata class.
+
+    Handles reading, modification and validation of data structure. Checks
+    hashes all the time.
+    '''
+    pass
+
+
+class TranslatorWrapper:
+    '''
+    Helper class to simplify class composition and wrapping properties
+
+    THIS IS NOT NECESSARY. SIMPLY DEFINE REQUIRED METHODS AND PROPERTIES IN
+    AlbumMetadata
+    '''
+    pass
+
+
+class AlbumMetadata:
+    '''
+    Stable API for music album metadata
+    '''
+    pass
