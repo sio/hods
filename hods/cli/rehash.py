02af0d228ca0acde207b4203478d4428ae96693b
Vitaly Potyarkin <sio.wtf@gmail.com>
2018-10-15 18:16:13 +0300

    Implement 'rehash' subcommand

--- hods/cli/rehash.py
+++ hods/cli/rehash.py
@@ -14,6 +14,29 @@
 
 import sys
 
+from hods import Metadata
+from hods._lib.files import get_files
+
 
 def main():
-    pass  # TODO
+    args = sys.argv + ['', '']
+    sections = []
+    all_sections = False
+
+    if args[2] == '--sections-all':
+        args.pop(2)
+        all_sections = True
+    elif args[2].startswith('--sections'):
+        sections = args[2].split('=')[1].split(',')
+        args.pop(2)
+
+    files = set(a for a in args[2:] if a)
+    if not files: files = get_files()
+
+    for filename in files:
+        meta = Metadata(filename=filename)
+        if all_sections:
+            sections = [x for x in meta if x != 'info']
+        meta.validate_hashes(sections=sections, write_updates=True)
+        meta.write()
+        print('Data hashes updated for: {}'.format(filename))
