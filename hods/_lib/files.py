a4aac45b33c0044117eb577aeea0edd477e52f5c
Vitaly Potyarkin <sio.wtf@gmail.com>
2018-10-16 14:58:10 +0300

    Add support for YAML metadata storage

--- hods/_lib/files.py
+++ hods/_lib/files.py
@@ -2,15 +2,22 @@
 File operations for HODS
 
 Supported file formats:
-    - StrictYAML - YAML files with .syml extension
     - JSON - .json extension
+    - StrictYAML - YAML files with .syml extension
+    - YAML - vanilla YAML files with .yml or .yaml extension
 '''
+# Checklist for adding support of new formats:
+#   - get_object
+#   - write_object
+#   - detect_format
+#   - module docsctring
 
 
 import os
 import json
 from collections import OrderedDict
 import strictyaml
+from ruamel import yaml
 
 
 def get_object(filename, fileformat=None):
@@ -20,6 +27,7 @@ def get_object(filename, fileformat=None):
     loaders = {
         'JSON':       load_json,
         'StrictYAML': load_strict_yaml,
+        'YAML':       load_yaml,
     }
     return loaders[fileformat](filename)
 
@@ -31,6 +39,7 @@ def write_object(obj, filename, fileformat=None):
     writers = {
         'JSON':       write_json,
         'StrictYAML': write_strict_yaml,
+        'YAML':       write_yaml,
     }
     return writers[fileformat](obj, filename)
 
@@ -49,11 +58,13 @@ def get_files(directory='.', recursive=False):
 
 
 def detect_format(filename):
-    _, extension = os.path.splitext(filename)
+    _, extension = os.path.splitext(filename.lower())
     if extension == '.syml':
         return 'StrictYAML'
     elif extension == '.json':
         return 'JSON'
+    elif extension in {'.yml', '.yaml'}:
+        return 'YAML'
     else:
         raise ValueError('can not detect file format for {}'.format(filename))
 
@@ -69,7 +80,12 @@ def is_metadata(filename):
 def load_strict_yaml(filename):
     with open(filename) as f:
         serialized = f.read()
-        strictyaml.load(serialized).data
+        return strictyaml.load(serialized).data
+
+
+def load_yaml(filename):
+    with open(filename) as f:
+        return yaml.load(f, Loader=yaml.RoundTripLoader)
 
 
 def load_json(filename):
@@ -81,6 +97,20 @@ def write_strict_yaml(data, filename):
     # TODO: https://github.com/crdoconnor/strictyaml/issues/43
     with open(filename, 'w') as f:
         f.write(strictyaml.as_document(data).as_yaml())
+
+
+# Dump OrderedDicts as simple dictionaries
+yaml.add_representer(OrderedDict, yaml.RoundTripDumper.represent_dict, Dumper=yaml.RoundTripDumper)
+
+def write_yaml(data, filename):
+    with open(filename, 'w') as f:
+        yaml.dump(
+            data,
+            f,
+            default_flow_style=False,
+            allow_unicode=True,
+            Dumper=yaml.RoundTripDumper,
+        )
 
 
 def write_json(data, filename):
