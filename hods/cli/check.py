b02a8e470d1eac80deed7b5000daf7f1ab5d404f
Vitaly Potyarkin <sio.wtf@gmail.com>
2018-10-16 16:41:53 +0300

    Add `check` subcommand for validating files

---
+++ hods/cli/check.py
@@ -0,0 +1,61 @@
+'''
+{hods} {subcommand} --recursive [FILENAME1] [FILENAME2] ...
+
+    Check hash values and validate schemas for metadata file(s).
+'''
+
+
+import sys
+from urllib.error import HTTPError
+
+from hods import (
+    Metadata,
+    ValidationError,
+    HashMismatchError,
+)
+from hods._lib.files import get_files
+
+
+RECURSIVE_FLAG = '--recursive'
+
+
+def main():
+    args = sys.argv + ['', '']
+
+    if RECURSIVE_FLAG in args:
+        recursive = True
+        args.pop(args.index(RECURSIVE_FLAG))
+    else:
+        recursive = False
+
+    files = set(a for a in args[2:] if a)
+    if not files: files = get_files(recursive=recursive)
+
+    exit_code = 0
+    for filename in files:
+        print('Checking {}: '.format(filename), end='')
+        meta = None
+        try:
+            meta = Metadata(filename=filename)
+        except FileNotFoundError:
+            print('FILE NOT FOUND')
+            exit_code = 1
+        except HTTPError:
+            print('HTTP ERROR')
+            exit_code = 1
+        except ValidationError:
+            print('SCHEMA ERROR')
+            exit_code = 1
+        except:
+            print('PARSE ERROR')
+            exit_code = 1
+
+        if not meta: continue
+
+        try:
+            meta.validate_hashes()
+            print('OK')
+        except HashMismatchError:
+            print('HASH ERROR')
+            exit_code = 1
+    sys.exit(exit_code)
