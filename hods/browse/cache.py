dac1370fe7fb99eff39efdeb34d144322029f0b6
Vitaly Potyarkin <sio.wtf@gmail.com>
2018-10-25 11:02:18 +0300

    Drop obsolete caches upon initialization

--- hods/browse/cache.py
+++ hods/browse/cache.py
@@ -3,7 +3,7 @@ Sqlite-backed cache for multiple HODS documents
 '''
 
 
-import os.path
+import os
 from contextlib import contextmanager
 
 from sqlalchemy import (
@@ -16,11 +16,13 @@ from sqlalchemy import (
     String,
     create_engine,
 )
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
     relationship,
     sessionmaker,
 )
+from sqlalchemy.pool import NullPool
 
 
 
@@ -28,15 +30,21 @@ class DocumentsReadOnlyCache:
     '''
     Caching and searching interface to a collection of HODS documents
     '''
+    SCHEMA_VERSION = ('version', '1')  # used to drop outdated cache files
 
 
     def __init__(self, cache_db):
         '''Initialize cache database in file provided by path'''
-        self.db = create_engine(
-            'sqlite:///{}'.format(os.path.abspath(cache_db))
-        )
-        Base.metadata.create_all(self.db)  # no-op if database is not new
+        self.filename = os.path.abspath(cache_db)
+        self.db = create_engine('sqlite:///{}'.format(self.filename))
         self.sessionmaker = sessionmaker(bind=self.db)
+        if not self._check_version():
+            self._reinit_db()
+
+
+    def __del__(self):
+        '''Clean up'''
+        self.db.dispose()  # clear the connections pool
 
 
     @contextmanager
@@ -51,6 +59,33 @@ class DocumentsReadOnlyCache:
             raise
         finally:
             short_session.close()
+
+
+    def _check_version(self):
+        '''Check that cache file uses current database schema'''
+        version_key, version_valid = self.SCHEMA_VERSION
+
+        with self.session() as session:
+            try:
+                version = session.query(Info).filter(Info.option == version_key).first()
+                return bool(version and version.value == version_valid)
+            except OperationalError:
+                return False
+
+
+    def _reinit_db(self):
+        '''(Re)initialize empty cache database'''
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
+        self.db = create_engine('sqlite:///{}'.format(self.filename))
+        Base.metadata.create_all(self.db)
+        self.sessionmaker = sessionmaker(bind=self.db)
+
+        version_key, version_valid = self.SCHEMA_VERSION
+        version = Info(option=version_key, value=version_valid)
+        with self.session() as session:
+            session.add(version)
 
 
     def add(self, filename=None, directory=None):
@@ -88,12 +123,11 @@ class Content(Base):
     '''Full contents for all files added to cache'''
     __tablename__ = 'content'
 
-    id         = Column(Integer, primary_key=True, autoincrement=True)
     path       = Column(
                     String,
                     ForeignKey('files.path', ondelete='CASCADE', onupdate='CASCADE')
                  )  # TODO: foreign_keys enforcement is disabled by default in sqlite
-    key        = Column(String)
+    key        = Column(String, primary_key=True)
     key_prefix = Column(String)
     key_suffix = Column(String)
     value      = Column(String)
--- tests/test_browser_cache.py
+++ tests/test_browser_cache.py
@@ -3,17 +3,31 @@ Tests for SQLite-backed cache engine
 '''
 
 
-from unittest import TestCase
+import os.path
+from tempfile import TemporaryDirectory
+from unittest import TestCase, skip
 
-from hods.browse.cache import DocumentsReadOnlyCache, File
+from hods.browse.cache import DocumentsReadOnlyCache, File, Content, Info
+
 
 
 class testCacheORM(TestCase):
 
+    @skip('Does not work. SQLite file is not freed and can not be deleted')
     def test_creating_database(self):
-        cache = DocumentsReadOnlyCache('/tmp/cache_file.db')
-        with cache.session() as session:
-            f = File(path='hello')
-            session.add(f)
-        import pdb; pdb.set_trace()
-
+        with TemporaryDirectory(prefix='hods_tests_') as tempdir:
+            cache = DocumentsReadOnlyCache(os.path.join(tempdir, 'cache.db'))
+            cache._reinit_db()
+            with cache.session() as session:
+                f = File(path='hello')
+                c = Content(path='hello', key='1')
+                c1 = Content(path='hello world', key='2')
+                session.add(f)
+                session.add(c)
+                session.add(c1)
+            with cache.session() as s:
+                f = s.query(File).first()
+                s.delete(f)
+            with cache.session() as s:
+                self.assertEqual(len(session.query(Content).all()), 1)
+            #del(cache)
