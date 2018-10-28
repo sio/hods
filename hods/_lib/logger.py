e929308b4a956b987d0281cb698ae311dca44104
Vitaly Potyarkin <sio.wtf@gmail.com>
2018-10-28 10:30:09 +0300

    Replace tabs with spaces

--- hods/_lib/logger.py
+++ hods/_lib/logger.py
@@ -11,11 +11,11 @@ _package = __name__.split('.')[0]  # top-level package
 log = logging.getLogger(_package)
 
 if not log.handlers:
-	handler = logging.StreamHandler()
-	handler.setLevel(logging.DEBUG)
-	log.addHandler(handler)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    log.addHandler(handler)
 
 if os.environ.get('HODS_DEBUG'):
-	log.setLevel(logging.DEBUG)
+    log.setLevel(logging.DEBUG)
 else:
-	log.setLevel(logging.INFO)
+    log.setLevel(logging.INFO)
